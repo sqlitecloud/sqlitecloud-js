3aca8529193cc9189413246303f5a2c8c249c292
Gioele Cantoni <gioele.cantoni@gmail.com>
2024-11-15 14:23:07 +0100

    browser test log success

--- examples/with-javascript-browser/test.cjs
+++ examples/with-javascript-browser/test.cjs
@@ -24,6 +24,7 @@ const { chromium, firefox, webkit } = require('playwright');
         const messageOutput = await (page.locator('#messages')).allInnerTexts();
         if (messageOutput[0] != '[{"CustomerId":1,"FirstName":"Luís","LastName":"Gonçalves","Company":"Embraer - Empresa Brasileira de Aeronáutica S.A.","Address":"Av. Brigadeiro Faria Lima, 2170","City":"São José dos Campos","State":"SP","Country":"Brazil","PostalCode":"12227-000","Phone":"+55 (12) 3923-5555","Fax":"+55 (12) 3923-5566","Email":"luisg@embraer.com.br","SupportRepId":3},{"CustomerId":2,"FirstName":"Leonie","LastName":"Köhler","Company":null,"Address":"Theodor-Heuss-Straße 34","City":"Stuttgart","State":null,"Country":"Germany","PostalCode":"70174","Phone":"+49 0711 2842222","Fax":null,"Email":"leonekohler@surfeu.de","SupportRepId":5},{"CustomerId":3,"FirstName":"François","LastName":"Tremblay","Company":null,"Address":"1498 rue Bélanger","City":"Montréal","State":"QC","Country":"Canada","PostalCode":"H2G 1A7","Phone":"+1 (514) 721-4711","Fax":null,"Email":"ftremblay@gmail.com","SupportRepId":3}]\nconnected') throw Error('Invalid query result');
 
+        console.log(`✅ ${browser.name()} with-javascript-browser test passed`);
         await window.close();
     });
 
