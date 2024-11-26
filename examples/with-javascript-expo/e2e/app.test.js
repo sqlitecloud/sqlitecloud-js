c7ff7ace6461badd42e0a759b13bab3714f0e16b
Gioele Cantoni <gioele.cantoni@gmail.com>
2024-11-26 18:17:23 +0100

    updated tests

--- examples/with-javascript-expo/e2e/app.test.js
+++ examples/with-javascript-expo/e2e/app.test.js
@@ -7,21 +7,24 @@ describe('mobile todo app test', () => {
     await device.reloadReactNative();
   });
 
-  it.only('should have welcome screen', async () => {
+  it('should show welcome screen', async () => {
+    await expect(element(by.text(/cover/i))).toBeVisible();
+
     await expect(element(by.text(/organize your/i))).toBeVisible();
     await expect(element(by.text(/tasks with sqlite/i))).toBeVisible();
     await expect(element(by.text(/designed for happiness, not just productivity./i))).toBeVisible();
     await expect(element(by.text(/enjoy a stress-free way to manage your day./i))).toBeVisible();
 
+    await expect(element(by.text(/get started/i))).toBeVisible();
   });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
+  it('should get categories from chinook', async () => {
+    await expect(element(by.text(/get started/i))).toBeVisible();
+    await element(by.text(/get started/i)).tap();
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+    await expect(element(by.text(/inbox/i))).toBeVisible();
+    await expect(element(by.text(/work/i))).toBeVisible();
+    await expect(element(by.text(/personal/i))).toBeVisible();
+    await waitFor(element(by.text(/expo_ci_do_not_delete/i))).toBeVisible().withTimeout(10000);
   });
 });
