de350f0b062a7838c88ca649c3b2644eada78a4e
Gioele Cantoni <gioele.cantoni@gmail.com>
2024-11-26 18:29:48 +0100

    rm detox not working fix expo

--- examples/with-javascript-expo/.detoxrc.js
+++ examples/with-javascript-expo/.detoxrc.js
@@ -23,7 +23,7 @@ module.exports = {
     'android.debug': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
-      build: 'cd android && ./gradlew :app:assembleDebug :app:assembleAndroidTest -DtestBuildType=debug && cd ..',
+      build: 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
       reversePorts: [
         8081
       ]
@@ -31,7 +31,7 @@ module.exports = {
     'android.release': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
-      build: 'cd android && ./gradlew :app:assembleRelease :app:assembleAndroidTest -DtestBuildType=release && cd ..'
+      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release'
     }
   },
   devices: {
