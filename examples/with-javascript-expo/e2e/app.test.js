da47c2d7a031d984df574c5ff3d971a35a54ae41
Gioele Cantoni <gioele.cantoni@gmail.com>
2024-11-26 18:48:27 +0100

    finished expo tests and re-enabled ios test to run them in parallel with android

--- examples/with-javascript-expo/e2e/app.test.js
+++ examples/with-javascript-expo/e2e/app.test.js
@@ -1,3 +1,16 @@
+//won't do insert and delete action in these tests since they will run in parallel in different devices
+
+//cross-compatible go back function
+export const pressBack = async () => {
+  if (device.getPlatform() === 'android') {
+    await device.pressBack();
+  } else {
+    await element(by.traits(['button']))
+      .atIndex(0)
+      .tap();
+  }
+};
+
 describe('mobile todo app test', () => {
   beforeAll(async () => {
     await device.launchApp();
@@ -26,5 +39,28 @@ describe('mobile todo app test', () => {
     await expect(element(by.text(/work/i))).toBeVisible();
     await expect(element(by.text(/personal/i))).toBeVisible();
     await waitFor(element(by.text(/expo_ci_do_not_delete/i))).toBeVisible().withTimeout(10000);
+  });
+
+  it('should get tasks from chinook', async () => {
+    await expect(element(by.text(/get started/i))).toBeVisible();
+    await element(by.text(/get started/i)).tap();
+
+    await expect(element(by.text(/inbox/i))).toBeVisible();
+    await element(by.text(/inbox/i)).tap();
+    await waitFor(element(by.text(/task_work/i))).toBeVisible().withTimeout(10000);
+    await waitFor(element(by.text(/task_personal/i))).toBeVisible().withTimeout(10000);
+    await pressBack();
+
+    await expect(element(by.text(/work/i))).toBeVisible();
+    await element(by.text(/work/i)).tap();
+    await waitFor(element(by.text(/task_work/i))).toBeVisible().withTimeout(10000);
+    await pressBack();
+
+    await expect(element(by.text(/personal/i))).toBeVisible();
+    await element(by.text(/personal/i)).tap();
+    await waitFor(element(by.text(/task_personal/i))).toBeVisible().withTimeout(10000);
+    await pressBack();
+
+    await expect(element(by.text(/categories/i))).toBeVisible();
   });
 });
