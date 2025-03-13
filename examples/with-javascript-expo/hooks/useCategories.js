64eda5e3a742d644e7adad4b379c00126fddee8a
Daniele Briggi <=>
2025-03-13 09:15:47 +0100

    fix(examples): missing close()

--- examples/with-javascript-expo/hooks/useCategories.js
+++ examples/with-javascript-expo/hooks/useCategories.js
@@ -1,78 +1,72 @@
-import { useState, useEffect } from "react";
-import getDbConnection from "../db/dbConnection";
+import { useState, useEffect } from 'react'
+import getDbConnection from '../db/dbConnection'
 
 const useCategories = () => {
-  const [moreCategories, setMoreCategories] = useState(["Work", "Personal"]);
+  const [moreCategories, setMoreCategories] = useState(['Work', 'Personal'])
 
   const getCategories = async () => {
+    let db = null;
     try {
-      const tags = await getDbConnection().sql("SELECT * FROM tags");
-      const filteredTags = tags.filter((tag) => {
-        return tag["name"] !== "Work" && tag["name"] !== "Personal";
-      });
-      setMoreCategories((prevCategories) => [
-        ...prevCategories,
-        ...filteredTags.map((tag) => tag.name),
-      ]);
+      const tags = await db.sql('SELECT * FROM tags')
+      const filteredTags = tags.filter(tag => {
+        return tag['name'] !== 'Work' && tag['name'] !== 'Personal'
+      })
+      setMoreCategories(prevCategories => [...prevCategories, ...filteredTags.map(tag => tag.name)])
     } catch (error) {
-      console.error("Error getting tags/categories", error);
+      console.error('Error getting tags/categories', error)
     }
-  };
+  }
 
-  const addCategory = async (newCategory) => {
+  const addCategory = async newCategory => {
+    let db = null;
     try {
-      await getDbConnection().sql(
-        "INSERT INTO tags (name) VALUES (?) RETURNING *",
-        newCategory
-      );
-      setMoreCategories((prevCategories) => [...prevCategories, newCategory]);
+      db = getDbConnection();
+      await db.sql('INSERT INTO tags (name) VALUES (?) RETURNING *', newCategory)
+      setMoreCategories(prevCategories => [...prevCategories, newCategory])
     } catch (error) {
-      console.error("Error adding category", error);
+      console.error('Error adding category', error)
+    } finally {
+      db?.close();
     }
-  };
+  }
 
   const initializeTables = async () => {
+    let db = null;
     try {
-      const createTasksTable = await getDbConnection().sql(
-        "CREATE TABLE IF NOT EXISTS tasks (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, isCompleted INT NOT NULL);"
-      );
+      db = getDbConnection();
+      const createTasksTable = await db.sql(
+        'CREATE TABLE IF NOT EXISTS tasks (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, isCompleted INT NOT NULL);'
+      )
 
-      const createTagsTable = await getDbConnection().sql(
-        "CREATE TABLE IF NOT EXISTS tags (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, UNIQUE(name));"
-      );
+      const createTagsTable = await db.sql('CREATE TABLE IF NOT EXISTS tags (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, UNIQUE(name));')
 
-      const createTagsTasksTable = await getDbConnection().sql(
-        "CREATE TABLE IF NOT EXISTS tasks_tags (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, task_id INTEGER NOT NULL, tag_id INTEGER NOT NULL, FOREIGN KEY (task_id) REFERENCES tasks(id), FOREIGN KEY (tag_id) REFERENCES tags(id));"
-      );
+      const createTagsTasksTable = await db.sql(
+        'CREATE TABLE IF NOT EXISTS tasks_tags (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, task_id INTEGER NOT NULL, tag_id INTEGER NOT NULL, FOREIGN KEY (task_id) REFERENCES tasks(id), FOREIGN KEY (tag_id) REFERENCES tags(id));'
+      )
 
-      if (
-        createTasksTable === "OK" &&
-        createTagsTable === "OK" &&
-        createTagsTasksTable === "OK"
-      ) {
-        console.log("Successfully created tables");
+      if (createTasksTable === 'OK' && createTagsTable === 'OK' && createTagsTasksTable === 'OK') {
+        console.log('Successfully created tables')
 
-        await getDbConnection().sql("INSERT OR IGNORE INTO tags (name) VALUES (?)", "Work");
-        await getDbConnection().sql(
-          "INSERT OR IGNORE INTO tags (name) VALUES (?)",
-          "Personal"
-        );
-        getCategories();
+        await db.sql('INSERT OR IGNORE INTO tags (name) VALUES (?)', 'Work')
+        await db.sql('INSERT OR IGNORE INTO tags (name) VALUES (?)', 'Personal')
+        getCategories()
       }
     } catch (error) {
-      console.error("Error creating tables", error);
+      console.error('Error creating tables', error)
+    } finally {
+      db?.close();
     }
-  };
+  }
 
   useEffect(() => {
-    initializeTables();
-  }, []);
+    initializeTables()
+  }, [])
 
   return {
     moreCategories,
     addCategory,
-    getCategories,
-  };
-};
+    getCategories
+  }
+}
 
-export default useCategories;
+export default useCategories
