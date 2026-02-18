e87d01135feabb8f1a14ebe85e252ba77c58e1b8
Gioele Cantoni <gioele.cantoni@gmail.com>
2026-02-18 20:08:08 +0100

    Update port to 443

--- src/drivers/types.ts
+++ src/drivers/types.ts
@@ -85,7 +85,7 @@ export interface SQLiteCloudConfig {
 
   /** True if we should force use of SQLite Cloud Gateway and websocket connections, default: true in browsers, false in node.js */
   usewebsocket?: boolean
-  /** Url where we can connect to a SQLite Cloud Gateway that has a socket.io deamon waiting to connect, eg. wss://host:4000 */
+  /** Url where we can connect to a SQLite Cloud Gateway that has a socket.io deamon waiting to connect, eg. wss://host:443 */
   gatewayurl?: string
 
   /** Optional identifier used for verbose logging */
